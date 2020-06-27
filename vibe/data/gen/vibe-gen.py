b38847ba2e975a4cbf3c608717f66fc97953b8a1
jakingy <jakingy.info@gmail.com>
2020-06-27 16:47:30 +1000

    Vibe cases

--- vibe/data/gen/vibe-gen.py
+++ vibe/data/gen/vibe-gen.py
@@ -2,7 +2,62 @@ import tree_gen as tg
 import sys
 import random
 
-N, M, tree_type, id_type = map(int, sys.argv[1:])
+usage = """
+Usage:
+To generate a random case for a subtask use:
+python vibe-gen.py <subtask>
+
+To specify additional parameters, use:
+python vibe-gen.py <subtask> <N> <M> <treetype> <idtype>
+
+case will be written to stdout
+
+subtask - the subtask of case (1-6)
+N - number of cities (2 - subtask limit)
+M - number of reporters (1 - subtask limit)
+treetype - the type of tree generated as per tree_gen.py (0-9)
+idtype - the method used to assign id's to the tree as per tree_gen.py (0-2)
+"""
+
+if len(sys.argv) < 2:
+    print(usage)
+
+if len(sys.argv) < 6:
+    subtask = int(sys.argv[1])
+else:
+    subtask, N, M, tree_type, id_type = map(int, sys.argv[1:])
+
+defaults = (int(1e5), int(1e6), int(1e6), list(range(len(tg.TREE_TYPES))))
+
+# if constraints are not specified, will generate them randomly
+
+MAX_N, MAX_M, MAX_di, allowed_types = (
+    (), # 1-based subtasks,
+    (100, defaults[1], 100, defaults[3]),
+    (1000, defaults[1], 1000, defaults[3]),
+    (defaults[0], 1, defaults[2], [0]),
+    (defaults[0], 1, defaults[2], defaults[3]),
+    (defaults[0], defaults[1], defaults[2], [0]),
+    defaults
+)[subtask]
+
+MAX_vi = int(1e6)
+
+if len(sys.argv) < 6:
+    N = random.randint(2, MAX_N)
+    M = random.randint(1, MAX_M)
+    tree_type = random.choice(allowed_types)
+    id_type = random.randrange(0,len(tg.ID_TYPES))
+
+# checks if valid subtask
+if N < 2 or N > MAX_N:
+    raise Exception("N out of bounds for subtask.")
+
+if M < 1 or M > MAX_M:
+    raise Exception("M out of bounds for subtask.")
+
+if  tree_type not in allowed_types:
+    raise Exception("Invalid tree type for subtask.")
 
 tree = tg.TREE_TYPES[tree_type](N)
 
@@ -22,6 +77,6 @@ def dfs(u):
 dfs(tree)
 
 print(N,M)
-print(*[random.randint(1, int(1e6)) for i in range(N)])
-print(*[random.randint(1, int(1e6)) for i in range(M)])
+print(*[random.randint(1, int(MAX_vi)) for i in range(N)])
+print(*[random.randint(1, int(MAX_di)) for i in range(M)])
 print(*parent[2:])
